b28413e75bf333fc32fe62deaed9b6d593a54067
Tob1Cont1nued <146166287+Tob1Cont1nued@users.noreply.github.com>
2025-12-11 17:35:10 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,20 @@
 import { Routes } from '@angular/router';
-import { renderMode } from '@angular/ssr';
 
 export const routes: Routes = [
   { 
     path: '', 
     loadComponent: () => import('./pages/start.component').then(m => m.StartComponent) 
   },
-
   { 
     path: 'frage/:index',
-    loadComponent: () => import('./pages/question.component').then(m => m.QuestionComponent),
-    // Wichtig: Prerendering für diese Parameter-Route deaktivieren
-    providers: [renderMode('server')] 
-    // Alternativ statt 'server': renderMode('client'), falls du kein SSR willst
+    loadComponent: () => import('./pages/question.component').then(m => m.QuestionComponent)
   },
-
   { 
     path: 'ergebnis', 
     loadComponent: () => import('./pages/result.component').then(m => m.ResultComponent) 
   },
-
-  { path: '**', redirectTo: '' }
+  { 
+    path: '**', 
+    redirectTo: '' 
+  }
 ];
-
